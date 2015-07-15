17a3648b35edf501173d2410d347362a614b3adc
Orion Edwards <orion.edwards@gmail.com>
2015-07-16 06:53:03 +1200

    Add basic web app to demo table builder

---
+++ WebApplication/Helpers/TableHelpers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication
+{
+    public static class TableHelpers
+    {
+        class TableBuilder<T>
+        {
+            struct ColumnDefinition
+            {
+                public string Title;
+                public Func<T, IHtmlString> Generator;
+            }
+
+            List<ColumnDefinition> columns = new List<ColumnDefinition>();
+
+            public void Column(string title, Func<T, string> contents)
+            {
+                columns.Add(new ColumnDefinition {
+                    Title = title,
+                    Generator = contents;
+                });
+            }
+
+            public void Column(string title, Func<T, IHtmlString> unsafeContents)
+            {
+                columns.Add(new ColumnDefinition
+                {
+                    Title = title,
+                    Generator = unsafeContents
+                });
+            }
+        }
+
+        public static IHtmlString DataTable<T>(IEnumerable<T> model, Action<TableBuilder<T>> builder)
+        {
+
+        }
+    }
+}
--- andand/Program.cs
+++ andand/Program.cs
@@ -4,26 +4,11 @@ namespace andand
 {
 	static class MainClass
 	{
+        class Person { public string Name => "Orion"; }
+
 		public static void Main(string[] args)
 		{
-			var result = GetBase()?.GetNext()?.GetNext();
-			Console.WriteLine("{0}", result);
 
-			Repeat(3, () => Console.WriteLine("Hello"));
-		}
-
-		static void Repeat(int n, Action action)
-		{
-			for(int i = 0; i < n; i++)
-				action();
-		}
-
-		static string GetBase() => null; //"Dog";
-
-		static string GetNext(this string x) {
-			var a = x.ToCharArray();
-			a[0] = (char)(a[0] + 1);
-			return new string(a);
-		}
-	}
+        }
+    }
 }
