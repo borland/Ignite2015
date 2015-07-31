8e6af96788567613a1d5722f4ae06fdbdf5910b6
Orion Edwards [GGL] <orione@gallagher.co>
2015-07-31 14:25:35 +1200

    Implement basic C# goroutines demo

---
+++ goroutines/basic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+class basic
+{
+    static async Task MultiplyAsync(int a, int b, Channel<int> result)
+        => await result.Send(a * b);
+
+    public static async void Run()
+    {
+        var result = new Channel<int>();
+        Go.Run(MultiplyAsync, 10, 20, result);
+        Thread.Sleep(1000);
+        Console.WriteLine($"result was {await result.Receive()}");
+    }
+}
+
--- goroutines/goroutines_main.cs
+++ goroutines/goroutines_main.cs
@@ -4,8 +4,9 @@ class MainClass
 {
     public static void Main(string[] args)
     {
+        basic.Run();
         //crc_basic.Run();
-        crc_concurrent.Run();
+        //crc_concurrent.Run();
 
         Console.ReadLine(); // need something to keep our process alive
     }
