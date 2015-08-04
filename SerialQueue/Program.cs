5ce0be1deb6d95b04f653eaa5e1d6a3ac9344668
Orion Edwards <orion.edwards@gmail.com>
2015-08-05 07:20:45 +1200

    Implement a SerialQueue and ThreadQueue to show memory and perf differences

--- SerialQueue/Program.cs
+++ SerialQueue/Program.cs
@@ -19,13 +19,130 @@
 // THE SOFTWARE.
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 class Program
 {
     static void Main(string[] args)
     {
+        const int NumQueues = 1;
+
+        Console.WriteLine($"About to create {NumQueues} queues"); Console.ReadLine();
+        var memory = GC.GetTotalMemory(forceFullCollection: true);
+
+        // create all the queues
+        var queues = new SerialQueue[NumQueues];
+        for(int i = 0; i < NumQueues; i++) {
+            queues[i] = new SerialQueue();
+        }
+
+        var memory2 = GC.GetTotalMemory(forceFullCollection: true);
+        var diff = (memory2 - memory) / 1024;
+
+        Console.WriteLine($"Queues consume {diff}KB; About to process events"); Console.ReadLine();
+        var sw = Stopwatch.StartNew();
+
+        // post all the events
+        var counters = new int[NumQueues];
+        for (int i = 0; i < NumQueues; i++) {
+            var loopi = i;
+            queues[i].DispatchAsync(() => {
+                counters[loopi]++;
+            });
+        }
+        for (int i = 0; i < NumQueues; i++) {
+            var loopi = i;
+            queues[i].DispatchAsync(() => {
+                counters[loopi]++;
+            });
+        }
+        for (int i = 0; i < NumQueues; i++) {
+            var loopi = i;
+            queues[i].DispatchSync(() => {
+                counters[loopi]++;
+            });
+        }
+        for(int i = 0; i < NumQueues; i++) {
+            if (counters[i] != 3) {
+                Console.WriteLine($"queue {i} broken!. hitcount was {counters[i]} instead of 3");
+            }
+        }
+        sw.Stop();
+
+        Console.WriteLine($"Processed in {sw.ElapsedMilliseconds}ms; about to dispose"); Console.ReadLine();
+
+        // dispose all the queues
+        foreach (var q in queues)
+            q.Dispose();
+    }
+}
+
+// manages a "queue" by posting events to a worker thread
+sealed class ThreadQueue : IDispatchQueue, IDisposable
+{
+    readonly Thread m_thread;
+    volatile bool m_isDisposed = false;
+    readonly List<Action> m_asyncActions = new List<Action>();
+
+    public ThreadQueue()
+    {
+        m_thread = new Thread(ThreadProc);
+        m_thread.IsBackground = true;
+        m_thread.Start();
+    }
+
+    public IDisposable DispatchAsync(Action action)
+    {
+        lock (m_asyncActions) {
+            m_asyncActions.Add(action);
+            Monitor.Pulse(m_asyncActions);
+        }
+
+        return new AnonymousDisposable(() => {
+            lock(m_asyncActions)
+                m_asyncActions.Remove(action);
+        });
+    }
+
+    public void DispatchSync(Action action)
+    {
+        var mre = new ManualResetEvent(false);
+        DispatchAsync(() => {
+            action();
+            mre.Set();
+        });
+        mre.WaitOne();
+    }
+
+    public void Dispose()
+    {
+        m_isDisposed = true;
+        lock (m_asyncActions) {
+            m_asyncActions.Clear();
+            Monitor.Pulse(m_asyncActions);
+        }
+    }
+
+    void ThreadProc()
+    {
+        while(!m_isDisposed) {
+            Action action = null;
+            lock (m_asyncActions) {
+                if (m_asyncActions.Count == 0)
+                    Monitor.Wait(m_asyncActions);
+
+                if (m_isDisposed || m_asyncActions.Count == 0) // either disposed or spurious wakeup
+                    continue;
+                
+                action = m_asyncActions[0];
+                m_asyncActions.RemoveAt(0);
+            }
+            Debug.Assert(action != null);
+            action();
+        }
     }
 }
--- SerialQueue/SerialQueue.cs
+++ SerialQueue/SerialQueue.cs
@@ -19,11 +19,118 @@
 // THE SOFTWARE.
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-class SerialQueue
+public interface IDispatchQueue
 {
+    IDisposable DispatchAsync(Action action);
+    void DispatchSync(Action action);
+}
 
+public class SerialQueue : IDispatchQueue, IDisposable
+{
+    // lock-order: We must always lock m_asyncActions BEFORE we lock
+    // m_executionLock. Once we hold m_executionLock we can release m_asyncActionsLock
+    readonly List<Action> m_asyncActions = new List<Action>();
+    volatile bool asyncActionsAreProcessing = false; // acquire m_asyncActions lock before making decisions
+    readonly object m_executionLock = new object();
+
+    public SerialQueue()
+    { }
+
+    public IDisposable DispatchAsync(Action action)
+    {
+        lock (m_asyncActions)
+            m_asyncActions.Add(action);
+
+        var cts = new CancellationTokenSource();
+        Task.Run(() => ProcessAsyncActions(), cts.Token);
+
+        return new AnonymousDisposable(() => {
+            cts.Cancel();
+            lock (m_asyncActions)
+                m_asyncActions.Remove(action);
+        });
+    }
+
+    void ProcessAsyncActions()
+    {
+        bool actionsLockTaken = false;
+        while (actionsLockTaken == false)
+            Monitor.Enter(m_asyncActions, ref actionsLockTaken);
+
+        if (asyncActionsAreProcessing)
+            return; // another thread is already processing, we don't want to waste this one sitting on m_executionLock
+        asyncActionsAreProcessing = true;
+        // even though we don't hold m_asyncActionsLock when asyncActionsAreProcessing is set to false
+        // that should be OK as the only "contention" happens up here while we do hold it
+
+        try {
+            while (m_asyncActions.Count > 0) {
+                lock (m_executionLock) {
+                    // get the head of the queue, then release the lock
+                    var action = m_asyncActions[0];
+                    m_asyncActions.RemoveAt(0);
+                    Monitor.Exit(m_asyncActions);
+                    actionsLockTaken = false;
+
+                    // process the action
+                    action();
+
+                    // now re-acquire the lock for the next thing
+                    while (actionsLockTaken == false)
+                        Monitor.Enter(m_asyncActions, ref actionsLockTaken);
+                }
+            }
+        }
+        finally {
+            asyncActionsAreProcessing = false;
+            if (actionsLockTaken)
+                Monitor.Exit(m_asyncActions);
+        }
+    }
+
+    public void DispatchSync(Action action)
+    {
+        bool actionsLockTaken = false;
+        while (actionsLockTaken == false)
+            Monitor.Enter(m_asyncActions, ref actionsLockTaken);
+
+        try {
+            if (m_asyncActions.Count == 0) {
+                lock (m_executionLock) {
+                    Monitor.Exit(m_asyncActions);
+                    actionsLockTaken = false;
+
+                    action();
+                }
+            }
+            else { // the queue is busy but we still hold the queue lock
+                var mre = new ManualResetEvent(false);
+                DispatchAsync(() => {
+                    action();
+                    mre.Set();
+                });
+                Monitor.Exit(m_asyncActions);
+                actionsLockTaken = false;
+
+                mre.WaitOne();
+            }
+        }
+        finally { // should never get here but inc ase we have some unepxectd throw
+            if (actionsLockTaken)
+                Monitor.Exit(m_asyncActions);
+        }
+    }
+
+    // dump the queue (which should cause any workers to stop after their current action)
+    public void Dispose()
+    {
+        lock (m_asyncActions)
+            m_asyncActions.Clear();
+    }    
 }
